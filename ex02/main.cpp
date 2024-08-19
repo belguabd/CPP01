eb37e5d45dc6671c31a1a3b698d714b3304ee26e
belguabd <brahim2001elguabdi@gmail.com>
2024-08-19 14:50:38 +0100

    init ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::string A = "HI THIS IS BRAIN";
+    std::cout << "Pointer Value : " << A << std::endl;
+    std::cout << "Pointer Address : " << &A << std::endl;
+}
