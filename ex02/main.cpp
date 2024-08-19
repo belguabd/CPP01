d45c7b7193ecb68d36fff8a20860af5e21ce4003
belguabd <brahim2001elguabdi@gmail.com>
2024-08-19 17:15:38 +0100

    finish the ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
+
+    
     std::string A = "HI THIS IS BRAIN";
-    std::cout << "Pointer Value : " << A << std::endl;
-    std::cout << "Pointer Address : " << &A << std::endl;
+    std::string *stringPTR = &A;
+    std::string &stringREF = A;
+    std::cout << "Memory Address of A : " << &A<< std::endl;
+    std::cout << "Memory Address of stringPTR : " << stringPTR<< std::endl;
+    std::cout << "Memory Address of stringREF : " << &stringREF<< std::endl;
+
+    std::cout << "Value of the string : " << A << std::endl;
+    std::cout << "Value of stringPTR : " << *stringPTR << std::endl;
+    std::cout << "Value of stringREF : " << stringREF << std::endl;
+
 }
